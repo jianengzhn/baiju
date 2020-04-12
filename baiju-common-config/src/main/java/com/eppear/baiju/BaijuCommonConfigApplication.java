7ffc984d706de1bda03df604bbd417a312b46c14
jianfengzhn <6955825@qq.com>
2020-04-12 22:22:18 +0800

    整理完了Gradle配置，完成了BANNER定制

---
+++ baiju-common-config/src/main/java/com/eppear/baiju/BaijuCommonConfigApplication.java
@@ -0,0 +1,13 @@
+package com.eppear.baiju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaijuCommonConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaijuCommonConfigApplication.class, args);
+    }
+
+}
