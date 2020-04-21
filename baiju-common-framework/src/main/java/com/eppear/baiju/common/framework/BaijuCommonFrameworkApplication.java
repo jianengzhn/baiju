ce40bdac454041483f0ead76d4a7df2364393763
边城高守 <6955825@qq.com>
2020-04-21 15:56:20 +0800

    新增了项目

---
+++ baiju-common-framework/src/main/java/com/eppear/baiju/common/framework/BaijuCommonFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.eppear.baiju.common.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaijuCommonFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaijuCommonFrameworkApplication.class, args);
+    }
+
+}
---
+++ baiju-common-framework/src/test/java/com/eppear/baiju/common/framework/BaijuCommonFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eppear.baiju.common.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaijuCommonFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
