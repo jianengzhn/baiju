45ea573a3b33a8813b13592ae29344056ac384cf
jianfengzhn <6955825@qq.com>
2020-04-21 23:09:01 +0800

    创建启动项目

---
+++ baiju-boostrap/src/main/java/com/eppear/baiju/BaijuBoostrapApplication.java
@@ -0,0 +1,13 @@
+package com.eppear.baiju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaijuBoostrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaijuBoostrapApplication.class, args);
+    }
+
+}
---
+++ baiju-boostrap/src/test/java/com/eppear/baiju/BaijuBoostrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eppear.baiju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaijuBoostrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
