527ef77538796fd1c6726e00cd59ab5a122f2d25
jianfengzhn <6955825@qq.com>
2020-05-06 07:13:18 +0800

    增加security相关配置

---
+++ baiju-boostrap/src/main/java/com/eppear/baiju/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.eppear.baiju.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
+
+/**
+ * Web服务中Security服务配置
+ * @author zjf
+ * @date 2020-05-03 16:26:11
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     *  与springSession整合记住我功能
+     * @return
+     */
+    @Bean
+    public SpringSessionRememberMeServices rememberMeServices() {
+        SpringSessionRememberMeServices rememberMeServices =
+                new SpringSessionRememberMeServices();
+        // optionally customize
+        rememberMeServices.setAlwaysRemember(true);
+        return rememberMeServices;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //配置认证方式等
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //http相关的配置，包括登入登出、异常处理、会话管理等
+        super.configure(http);
+    }
+
+}
