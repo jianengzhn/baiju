87febcf5ffb47a090a529e29b4e68e63d21aa234
jianfengzhn <6955825@qq.com>
2020-06-11 23:06:16 +0800

    整理了自动生成功能的代码

--- baiju-backstage-dao/src/main/java/com/eppear/baiju/backstage/dao/mapper/auto/TSysUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.eppear.baiju.backstage.dao.mapper.auto;
-
-import com.eppear.baiju.backstage.dao.config.MyMapper;
-import com.eppear.baiju.backstage.dao.model.auto.TSysUser;
-
-public interface TSysUserMapper extends MyMapper<TSysUser> {
-}
--- baiju-common-mgr/src/main/java/com/eppear/baiju/common/mgr/BaijuCommonMgrApplication.java
+++ baiju-common-mgr/src/main/java/com/eppear/baiju/common/mgr/BaijuCommonMgrApplication.java
@@ -3,51 +3,31 @@ package com.eppear.baiju.common.mgr;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
-import org.mybatis.generator.exception.InvalidConfigurationException;
-import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
-import java.io.IOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *
+ * @author zjf
+ */
 @SpringBootApplication
 public class BaijuCommonMgrApplication {
 
-    public static void main(String[] args) {
-        //SpringApplication.run(BaijuCommonMgrApplication.class, args);
+    public static void main(String[] args) throws Exception {
         List<String> warnings = new ArrayList<String>();
+
         boolean overwrite = true;
-        File configFile = new File("./baiju-common-mgr/src/main/resources/mybatis/generatorConfig.xml");
+        File configFile = new File("baiju-common-mgr/src/main/resources/mybatis/generatorConfig.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
-        Configuration config = null;
-        try {
-            config = cp.parseConfiguration(configFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (XMLParserException e) {
-            e.printStackTrace();
-        }
+        Configuration config = cp.parseConfiguration(configFile);
+
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-        MyBatisGenerator myBatisGenerator = null;
-        try {
-            myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-        } catch (InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
-        try {
-            myBatisGenerator.generate(null);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        MyBatisGenerator myBatisGenerator =  new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
     }
 
 }
